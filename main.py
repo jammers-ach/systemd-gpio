90fcea1028013a5465f0e21f3f7374fa5cc4bdbe
James <james@mathews.top>
2019-12-01 18:34:33 +0200

    integrated to raspberry gpio

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from config import load_config
 import systemwatcher
 import logging
+import RPi.GPIO as GPIO
+import time
 
 logger = logging.getLogger(__name__)
 
 def _main(filename):
     config = load_config(filename)
+    GPIO.setmode(GPIO.BOARD)
+
     watchers = []
     for s in config['services']:
         sw = systemwatcher.ServiceWatcher(s['name'], s['input'], s['output'])
+
+    try:
+        while True:
+            time.sleep(60)
+            print('sleeping')
+    finally:
+        GPIO.cleanup()
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
--- systemwatcher.py
+++ systemwatcher.py
@@ -5,6 +5,8 @@ import subprocess
 import logging
 import time
 
+import RPi.GPIO as GPIO
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,13 +23,23 @@ class ServiceWatcher():
     and if it differs from the actual status it will change it
     '''
     flash_freq = 0.1 # interval in seconds of flashing while service is canging
-    service_timeout = 60 # seconds while we're waiting for the service status to change
+    service_timeout = 15 # seconds while we're waiting for the service status to change
 
     def __init__(self, procname, sw_port, led_port):
         self.procname = procname
         self.sw_port = sw_port
         self.led_port = led_port
+
+        GPIO.setup(sw_port, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(led_port, GPIO.OUT, initial=GPIO.LOW)
+
         self.update_service()
+
+        GPIO.add_event_detect(sw_port,
+                              GPIO.BOTH,
+                              callback=self.switch_callback,
+                              bouncetime=200)
+
 
     @property
     def _system_status(self):
@@ -39,28 +51,39 @@ class ServiceWatcher():
         for line in output.split('\n'):
             if line.strip().startswith('Active: '):
                 status = line.split()[1]
-                logger.debug(line)
                 return status
         raise Exception('Could not get status for {}'.format(self.procname))
 
 
     def _stop_service(self):
-        output = subprocess.run(['service', self.procname, 'stop'],
-                       stdout=subprocess.PIPE).stdout.decode('utf-8')
+        logger.info("Stopping {}".format(self.procname))
+        subprocess.Popen(['service', self.procname, 'stop'])
         self._watch_loop('inactive')
-        logger.debug(output)
 
     def _start_service(self):
-        output = subprocess.run(['service', self.procname, 'start'],
-                       stdout=subprocess.PIPE).stdout.decode('utf-8')
+        logger.info("Starting {}".format(self.procname))
+        subprocess.Popen(['service', self.procname, 'start'])
         self._watch_loop('active')
-        logger.debug(output)
+
+
+    def switch_callback(self, port):
+        '''
+        callback for swtiches
+        '''
+        logger.debug("SWITCH")
+        if port == self.sw_port:
+            self.update_service()
+        else:
+            logger.error("Got interupt on {}, but {} is on {}".format(port, self.procname, self.sw_port))
 
     def update_service(self):
         '''Sets the service status to match the switch status'''
-        if self.switch_status and self._system_status == 'inactive':
+        service_status = self._system_status
+        switch_status = self.switch_status
+        logger.debug("Switch: {}, Service: {}".format(switch_status, service_status))
+        if switch_status and service_status == 'inactive':
             self._start_service()
-        elif not self.switch_status and self._system_status != 'inactive':
+        elif not switch_status and service_status != 'inactive':
             self._stop_service()
 
 
@@ -68,27 +91,27 @@ class ServiceWatcher():
         '''Loops '''
         count = 0
         led_state = True
+        self.update_led(desired_state == 'active')
         while self._system_status != desired_state:
-            self.update_led(led_state)
             led_state = not led_state
             time.sleep(self.flash_freq)
             count += 1
             if count > (self.service_timeout/self.flash_freq):
                 raise Exception("timed out waiting for {}".format(self.procname))
-        self.update_led(self._system_status == 'active')
-
+        self.update_led(desired_state == 'active')
 
     def update_led(self, state):
         '''sets the LEDs state: true for on false for off
         '''
-        print('LED: ', state)
+        logger.debug('LED: {} '.format((state)))
+        GPIO.output(self.led_port, 1 if state else 0)
 
     @property
     def switch_status(self):
         '''returns true if switch is on (i.e. the service
         should be on)
         '''
-        return True
+        return GPIO.input(self.sw_port) == 1
 
 # All GPIO related work is based on
 # http://raspberry.io/projects/view/reading-and-writing-from-gpio-ports-from-python/
@@ -97,6 +120,14 @@ class ServiceWatcher():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    s = ServiceWatcher('openvpn', 20, 20)
+    GPIO.setmode(GPIO.BOARD)
 
-    print(s._system_status)
+    s = ServiceWatcher('bluetooth', 11, 12)
+
+    try:
+        while True:
+            time.sleep(5)
+            print(s._system_status)
+    finally:
+        GPIO.cleanup()
+
